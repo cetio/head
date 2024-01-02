02408e94b55cef2182319ec0692a724ce5e5205c
cetio <qubbinyt@gmail.com>
2024-01-01 21:33:41 -0500

    Improved encryption

--- source/app.d
+++ source/app.d
@@ -1,12 +1,23 @@
 import std.stdio;
 import godwit.stream;
 import godwit.formats;
-import godwit.encryption;
+import godwit.bse;
 import std.datetime;
 import crypto.aes;
 import crypto.padding;
+import std.conv;
 
 void main()
 {
-	
+	Stream stream = new Stream(r"C:\Users\stake\Downloads\test.exe");
+    writeln(stream.data.length / 1024 / 1024, "MB");
+    auto start = Clock.currTime;
+    stream.encrypt("yKxPczCgvMp94Bn3NsVf28m6rjqwUD5t");
+    auto end = Clock.currTime;
+    writeln("BSE256 ", (end - start).split!("msecs").msecs, "ms");
+    ubyte[] iv = [1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 1, 2, 3, 4];
+    start = Clock.currTime;
+    AESUtils.encrypt!AES256(stream.data, "yKxPczCgvMp94Bn3NsVf28m6rjqwUD5t", iv, PaddingMode.PKCS5);
+    end = Clock.currTime;
+    writeln("AES256 ", (end - start).split!("msecs").msecs, "ms");
 }
--- source/bse.d
+++ source/bse.d
@@ -1,26 +1,11 @@
-module bse;
+module godwit.bse;
 
 import std.traits;
 import std.parallelism;
+import core.simd;
 
 public:
 static:
-/*const int[] seed = [
-    0x50a7f451, 0x5365417e, 0xc3a4171a, 0x965e273a, 0xcb6bab3b, 
-    0xf1459d1f, 0xab58faac, 0x9303e34b, 0x55fa3020, 0xf66d76ad,
-    0x9176cc88, 0x254c02f5, 0xfcd7e54f, 0xd7cb2ac5, 0x80443526, 
-    0x8fa362b5, 0x495ab1de, 0x671bba25, 0x980eea45, 0xe1c0fe5d,
-    0x02752fc3, 0x12f04c81, 0xa397468d, 0xc6f9d36b, 0xe75f8f03, 
-    0x959c9215, 0xeb7a6dbf, 0xda595295, 0x2d83bed4, 0xd3217458,
-    0x2969e049, 0x44c8c98e, 0x6a89c275, 0x78798ef4, 0x6b3e5899, 
-    0xdd71b927, 0xb64fe1be, 0x17ad88f0, 0x66ac20c9, 0xb43ace7d
-];*/
-ubyte[32] rand = [
-    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
-    0x0A, 0x0D, 0x1A, 0x1D, 0x2E, 0x2C, 0x13, 0x09,
-    0x1B, 0x26, 0x7F, 0x2F, 0x12, 0x19, 0x0B, 0x22, 
-    0x10, 0x01, 0x2A, 0x6F, 0x05, 0x14, 0x1E, 0x18,
-];
 /*void bse256ag_encrypt(ref ubyte[] bytes, string key)
 {
     if (key.length != 32)
@@ -40,8 +25,8 @@ ubyte[32] rand = [
     foreach (i, _; parallel(bytes))
     {
         ulong ri = ~i;
-        bytes[i] ^= cast(ubyte)((c >> i) & ri); 
-        bytes[i] ^= cast(ubyte)((d >> i) & ri);
+        bytes[i] ^= cast(ubyte)((c << i) & ri); 
+        bytes[i] ^= cast(ubyte)((d << i) & ri);
         bytes[i] += ri; 
         bytes[i] ^= cast(ubyte)((a << i) & ri); 
         bytes[i] ^= cast(ubyte)((b << i) & ri); 
@@ -65,86 +50,122 @@ ubyte[32] rand = [
         *block1 = *block2;
         *block2 = u0;
     }
-}
-
-void bse256ag_decrypt(ref ubyte[] bytes, string key)
-{
-    if (key.length != 32 || bytes.length % 8 != 0)
-        return;
-
-    ulong a = (cast(ulong*)&key[0])[0];
-    ulong b = (cast(ulong*)&key[0])[1];
-    ulong c = (cast(ulong*)&key[0])[2];
-    ulong d = (cast(ulong*)&key[0])[3];
-
-    ulong numBlocks = bytes.length / 8; 
-    foreach_reverse (i; 0..numBlocks) 
-    {
-        ulong* block1 = cast(ulong*)(bytes.ptr + (i * 8));
-        ulong* block2 = cast(ulong*)(bytes.ptr + (key[rand[i % 32] % key.length] % bytes.length));
-
-        ulong u0 = *block1;
-        *block1 = *block2;
-        *block2 = u0;
-    }
-
-    foreach_reverse (i; 0..bytes.length)
-    {
-        int ii = key[rand[i % 32] % key.length] % bytes.length;
-        ubyte b0 = bytes[ii];
-        bytes[ii] = bytes[i];
-        bytes[i] = b0;
-    }
-
-    foreach_reverse (i; 0..bytes.length)
-    {
-        ulong ri = ~i;
-        bytes[i] ^= cast(ubyte)((b << i) & ri); 
-        bytes[i] ^= cast(ubyte)((a << i) & ri); 
-        bytes[i] -= ri; 
-        bytes[i] ^= cast(ubyte)((d >> i) & ri); 
-        bytes[i] ^= cast(ubyte)((c >> i) & ri);
-    }
-
-    import std.stdio;
-    writeln(bytes);
-    readln();
-    bytes = bytes[0..($-*cast(uint*)&bytes[$-4])];
 }*/
 
 void bse256_encrypt(ubyte* ptr, int length, string key)
 {
     if (key.length != 32)
-        return;
+    {
+        import std.conv;
+        throw new Exception("Key is "~(key.length * 8).to!string~" bits, expected 256!");
+    }
+
+    // Steps:
+    // Create minikeys
+    // Partition the data into blocks
+    // Substitute blocks
+    // Encrypt each block, using the minikeys
+    // Shuffle the blocks
+    // Profit
 
     ulong a = (cast(ulong*)&key[0])[0];
     ulong b = (cast(ulong*)&key[0])[1];
     ulong c = (cast(ulong*)&key[0])[2];
     ulong d = (cast(ulong*)&key[0])[3];
 
-    foreach (i, _; parallel(ptr[0..length]))
-    {
-        ulong ri = ~i;
-        ptr[i] ^= cast(ubyte)((c >> i) & ri); 
-        ptr[i] ^= cast(ubyte)((d >> i) & ri);
-        ptr[i] -= ri; 
-        ptr[i] ^= cast(ubyte)((a << i) & ri); 
-        ptr[i] ^= cast(ubyte)((b << i) & ri); 
-    }
-    
     foreach (i; 0..length)
     {
-        int ii = key[rand[i % 32] % key.length] % length;
-        ubyte b0 = ptr[ii];
-        ptr[ii] = ptr[i];
-        ptr[i] = b0;
+        int ii = cast(int)(((c << i) & (d << i)) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
+    }
+
+    // This is on average 5ms faster than an if
+    foreach (r; 0..4)
+    switch (length % 16)
+    {
+        case 0:
+            foreach (i, _; parallel(ptr[0..(length / 16)]))
+            {
+                ulong2* vptr = cast(ulong2*)(ptr + (i * 16));
+            
+                ulong ri = ~i;
+                *vptr ^= (c << i) & ri;  
+                *vptr ^= (d << i) & ri; 
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 8:
+            foreach (i, _; parallel(ptr[0..(length / 8)]))
+            {
+                ulong* vptr = cast(ulong*)(ptr + (i * 8));
+            
+                ulong ri = ~i;
+                *vptr ^= (c << i) & ri;  
+                *vptr ^= (d << i) & ri; 
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 4:
+            foreach (i, _; parallel(ptr[0..(length / 4)]))
+            {
+                uint* vptr = cast(uint*)(ptr + (i * 4));
+                
+                ulong ri = ~i;
+                *vptr ^= (c << i) & ri;  
+                *vptr ^= (d << i) & ri; 
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 2:
+            foreach (i, _; parallel(ptr[0..(length / 2)]))
+            {
+                ushort* vptr = cast(ushort*)(ptr + (i * 2));
+                
+                ulong ri = ~i;
+                *vptr ^= (c << i) & ri;  
+                *vptr ^= (d << i) & ri; 
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        default:
+            foreach (i, _; parallel(ptr[0..length]))
+            {
+                ulong ri = ~i;
+                ptr[i] ^= (c << i) & ri;  
+                ptr[i] ^= (d << i) & ri; 
+                ptr[i] -= ri; 
+                ptr[i] ^= (a << i) & ri; 
+                ptr[i] ^= (b << i) & ri; 
+            }
+            break;
+    } 
+
+    foreach (i; 0..length)
+    {
+        int ii = cast(int)(((a << i) & (b << i)) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
     }
 }
 
 void bse256_decrypt(ubyte* ptr, int length, string key)
 {
     if (key.length != 32)
-        return;
+    {
+        import std.conv;
+        throw new Exception("Key is "~(key.length * 8).to!string~" bits, expected 256!");
+    }
 
     ulong a = (cast(ulong*)&key[0])[0];
     ulong b = (cast(ulong*)&key[0])[1];
@@ -153,74 +174,269 @@ void bse256_decrypt(ubyte* ptr, int length, string key)
 
     foreach_reverse (i; 0..length)
     {
-        int ii = key[rand[i % 32] % key.length] % length;
+        int ii = cast(int)(((a << i) & (b << i)) % length);
         ubyte b0 = ptr[i];
         ptr[i] = ptr[ii];
         ptr[ii] = b0;
     }
 
+    foreach (r; 0..4)
+    switch (length % 16)
+    {
+        case 0:
+            foreach_reverse (i; 0..(length / 16))
+            {
+                ulong2* vptr = cast(ulong2*)(ptr + (i * 16));
+            
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+                *vptr ^= (d << i) & ri; 
+                *vptr ^= (c << i) & ri; 
+            }
+            break;
+        case 8:
+            foreach_reverse (i; 0..(length / 8))
+            {
+                ulong* vptr = cast(ulong*)(ptr + (i * 8));
+            
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+                *vptr ^= (d << i) & ri; 
+                *vptr ^= (c << i) & ri; 
+            }
+            break;
+        case 4:
+            foreach_reverse (i; 0..(length / 4))
+            {
+                uint* vptr = cast(uint*)(ptr + (i * 4));
+                
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+                *vptr ^= (d << i) & ri; 
+                *vptr ^= (c << i) & ri; 
+            }
+            break;
+        case 2:
+            foreach_reverse (i; 0..(length / 2))
+            {
+                ushort* vptr = cast(ushort*)(ptr + (i * 2));
+                
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+                *vptr ^= (d << i) & ri; 
+                *vptr ^= (c << i) & ri; 
+            }
+            break;
+        default:
+            foreach_reverse (i; 0..length)
+            {
+                ulong ri = ~i;
+                ptr[i] ^= (b << i) & ri;  
+                ptr[i] ^= (a << i) & ri; 
+                ptr[i] += ri; 
+                ptr[i] ^= (d << i) & ri; 
+                ptr[i] ^= (c << i) & ri; 
+            }
+            break;
+    } 
+
     foreach_reverse (i; 0..length)
     {
-        ulong ri = ~i;
-        ptr[i] ^= cast(ubyte)((b << i) & ri); 
-        ptr[i] ^= cast(ubyte)((a << i) & ri); 
-        ptr[i] += ri; 
-        ptr[i] ^= cast(ubyte)((d >> i) & ri);
-        ptr[i] ^= cast(ubyte)((c >> i) & ri); 
-        
+        int ii = cast(int)(((c << i) & (d << i)) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
     }
 }
 
-void bse128_encrypt(ubyte* ptr, int length, string key)
+/* void bse128_encrypt(ubyte* ptr, int length, string key)
 {
     if (key.length != 16)
-        return;
+    {
+        import std.conv;
+        throw new Exception("Key is "~(key.length * 8).to!string~" bits, expected 128!");
+    }
 
-    //Encryption time:570ms
-    //ulong2 vec = (cast(ulong2*)&key[0])[0];
+    // Steps:
+    // Create minikeys
+    // Partition the data into blocks
+    // Substitute blocks
+    // Encrypt each block, using the minikeys
+    // Shuffle the blocks
+    // Profit
+
     ulong a = (cast(ulong*)&key[0])[0];
     ulong b = (cast(ulong*)&key[0])[1];
 
-    foreach (i, _; parallel(ptr[0..length]))
-    {
-        ptr[i] -= ~i; 
-        ptr[i] ^= cast(ubyte)((a << i) & ~i); 
-        ptr[i] ^= cast(ubyte)((b << i) & ~i); 
-        //ptr[i] ^= cast(ulong2)__simd_ib(XMM.PSLLDQ, vec, 31) & ~i;
-    }
-    
     foreach (i; 0..length)
     {
-        int ii = key[rand[i % 32] % key.length] % length;
-        ubyte b0 = ptr[ii];
-        ptr[ii] = ptr[i];
-        ptr[i] = b0;
+        int ii = cast(int)((a << i) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
+    }
+
+    // This is on average 5ms faster than an if
+    foreach (r; 0..4)
+    switch (length % 16)
+    {
+        case 0:
+            foreach (i, _; parallel(ptr[0..(length / 16)]))
+            {
+                ulong2* vptr = cast(ulong2*)(ptr + (i * 16));
+            
+                ulong ri = ~i;
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 8:
+            foreach (i, _; parallel(ptr[0..(length / 8)]))
+            {
+                ulong* vptr = cast(ulong*)(ptr + (i * 8));
+            
+                ulong ri = ~i;
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 4:
+            foreach (i, _; parallel(ptr[0..(length / 4)]))
+            {
+                uint* vptr = cast(uint*)(ptr + (i * 4));
+                
+                ulong ri = ~i;
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        case 2:
+            foreach (i, _; parallel(ptr[0..(length / 2)]))
+            {
+                ushort* vptr = cast(ushort*)(ptr + (i * 2));
+                
+                ulong ri = ~i;
+                *vptr -= ri; 
+                *vptr ^= (a << i) & ri; 
+                *vptr ^= (b << i) & ri; 
+            }
+            break;
+        default:
+            foreach (i, _; parallel(ptr[0..length]))
+            {
+                ulong ri = ~i;
+                ptr[i] -= ri; 
+                ptr[i] ^= (a << i) & ri; 
+                ptr[i] ^= (b << i) & ri; 
+            }
+            break;
+    } 
+
+    foreach (i; 0..length)
+    {
+        int ii = cast(int)((b << i) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
     }
 }
 
 void bse128_decrypt(ubyte* ptr, int length, string key)
 {
     if (key.length != 16)
-        return;
+    {
+        import std.conv;
+        throw new Exception("Key is "~(key.length * 8).to!string~" bits, expected 128!");
+    }
 
     ulong a = (cast(ulong*)&key[0])[0];
     ulong b = (cast(ulong*)&key[0])[1];
 
     foreach_reverse (i; 0..length)
     {
-        int ii = key[rand[i % 32] % key.length] % length;
+        int ii = cast(int)((a << i) % length);
         ubyte b0 = ptr[i];
         ptr[i] = ptr[ii];
         ptr[ii] = b0;
     }
 
+    foreach (r; 0..4)
+    switch (length % 16)
+    {
+        case 0:
+            foreach_reverse (i; 0..(length / 16))
+            {
+                ulong2* vptr = cast(ulong2*)(ptr + (i * 16));
+            
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+            }
+            break;
+        case 8:
+            foreach_reverse (i; 0..(length / 8))
+            {
+                ulong* vptr = cast(ulong*)(ptr + (i * 8));
+            
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+            }
+            break;
+        case 4:
+            foreach_reverse (i; 0..(length / 4))
+            {
+                uint* vptr = cast(uint*)(ptr + (i * 4));
+                
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+            }
+            break;
+        case 2:
+            foreach_reverse (i; 0..(length / 2))
+            {
+                ushort* vptr = cast(ushort*)(ptr + (i * 2));
+                
+                ulong ri = ~i;
+                *vptr ^= (b << i) & ri;  
+                *vptr ^= (a << i) & ri; 
+                *vptr += ri; 
+            }
+            break;
+        default:
+            foreach_reverse (i; 0..length)
+            {
+                ulong ri = ~i;
+                ptr[i] ^= (b << i) & ri;  
+                ptr[i] ^= (a << i) & ri; 
+                ptr[i] += ri; 
+            }
+            break;
+    } 
+
     foreach_reverse (i; 0..length)
     {
-        ptr[i] ^= cast(ubyte)((b << i) & ~i); 
-        ptr[i] ^= cast(ubyte)((a << i) & ~i); 
-        ptr[i] += ~i; 
+        int ii = cast(int)((b << i) % length);
+        ubyte b0 = ptr[i];
+        ptr[i] = ptr[ii];
+        ptr[ii] = b0;
     }
-}
+} */
 
 T bse_encrypt(T)(T val, string key)
     if (!isArray!T)
